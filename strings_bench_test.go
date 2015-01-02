e100ace8ff039dbb85f9595ffd41e2a6e8cef35c
Naoya Inada <naoina@kuune.org>
2015-01-02 18:38:44 +0900

    Add benchmarks

---
+++ strings_bench_test.go
@@ -0,0 +1,29 @@
+package stringutil_test
+
+import (
+	"testing"
+
+	"github.com/naoina/go-stringutil"
+)
+
+var benchcaseForCamelCase = "the_quick_brown_fox_jumps_over_the_lazy_dog"
+
+func BenchmarkToCamelCase(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		stringutil.ToCamelCase(benchcaseForCamelCase)
+	}
+}
+
+var benchcaseForSnakeCase = "TheQuickBrownFoxJumpsOverTheLazyDog"
+
+func BenchmarkToSnakeCase(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		stringutil.ToSnakeCase(benchcaseForSnakeCase)
+	}
+}
+
+func BenchmarkToSnakeCaseASCII(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		stringutil.ToSnakeCaseASCII(benchcaseForSnakeCase)
+	}
+}
